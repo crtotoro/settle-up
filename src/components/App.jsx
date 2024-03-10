5883d00edc2c35e572513b18e6308ed10a6a5a42
leaky3 <crtotoro@gmail.com>
2024-03-10 17:00:35 -0500

    Add intial input form to page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
 
 function App() {
   
@@ -6,6 +8,29 @@ function App() {
   return (
     <>
       <h1>Settle Up</h1>
+      <p>Split finances made easy</p>
+      <Stack direction="horizontal" gap={3}>
+        <Form.Group controlId="statementCsv" >
+          <Form.Label><b>Step 1.</b> Upload your credit card statement</Form.Label>
+          <Form.Control type="file" />
+          <Form.Text className='text-muted'>.csv format only</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="dateRange" >
+          <Form.Label><b>Step 2.</b> Choose start and end date </Form.Label>
+          <Stack direction='horizontal' gap={3}>
+            <Stack>
+              <Form.Control controlId="startDate" type="date" />
+              <Form.Text className="text-muted p2" >Start Date</Form.Text>
+            </Stack>
+            <Stack>
+              <Form.Control controlId="endDate" type="date" />
+              <Form.Text className="text-muted p2">End Date</Form.Text>
+            </Stack>
+          </Stack>
+        </Form.Group>
+
+      </Stack>
+
     </>
   )
 }
