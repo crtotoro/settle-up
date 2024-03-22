58b848f2eefa32efb3ab38a0ffe304d3f694f447
leaky3 <crtotoro@gmail.com>
2024-03-22 14:54:18 -0500

    Convert file state into signal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,12 @@
-
 import FileProcessor from './FileProcessor';
 
-
-import { InputGroup } from 'react-bootstrap';
-import { useEffect, useRef, useState } from 'react';
-
 function App() {
-  const [inputFiles, setInputFiles] = useState(null);
-  const [file, setFile] = useState(null);
-  const fileInputRef = useRef(null)
-
-  const handleUpload = () => {
-    if(inputFiles && inputFiles.length) {
-      setFile(inputFiles[0]);
-      setInputFiles(null);
-      fileInputRef.current.value = "";
-    }
-    
-  }
-
-  useEffect(() => {inputFiles && console.log("File ready for upload:", inputFiles[0].name)}, [inputFiles])
-  useEffect(() => {file && console.log("File uploaded:", file.name)}, [file])
-
+  
   return (
     <div className='m-3'>
       <h1>Settle Up</h1>
       <p>Split finances made easy</p>
-      <Stack direction='horizontal' gap={3}>
-        <Form.Group >
-          <Form.Label><b>Step 1.</b> Upload your credit card statement</Form.Label>
-          <InputGroup>
-            <Form.Control ref={fileInputRef} type='file' onChange={(e) => setInputFiles(e.target.files)} />
-            {inputFiles && inputFiles.length ? <Button variant='primary' onClick={handleUpload}>Upload</Button> : <Button variant='outline-secondary' disabled>Upload</Button>}
-          </InputGroup>
-          <Form.Text className='text-muted'>.csv format only</Form.Text>
-        </Form.Group>
-        <Form.Group id='date-range'>
-          <Form.Label><b>Step 2.</b> Choose start and end date </Form.Label>
-          <Stack direction='horizontal' gap={3}>
-            <Stack>
-              <Form.Control id='start-date' type='date'/>
-              <Form.Text className='text-muted p2' >Start Date</Form.Text>
-            </Stack>
-            <Stack>
-              <Form.Control id='end-date' type='date' />
-              <Form.Text className='text-muted p2'>End Date</Form.Text>
-            </Stack>
-          </Stack>
-        </Form.Group>
-
-      </Stack>
+      <FileProcessor/>
 
     </div>
   )
--- src/components/FileProcessor.jsx
+++ src/components/FileProcessor.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import Stack from 'react-bootstrap/Stack';
-import Form from 'react-bootstrap/Form';
+import React, { useEffect, useRef, useState } from 'react'
+import { InputGroup, Button, Form, Stack } from 'react-bootstrap';
 import Papa from 'papaparse';
 import moment from 'moment';
 import { isAfterGivenDate, isMatch } from '../utils/helpers';
+import { signal } from '@preact/signals-react';
 
 
 const initConfirmed = [
@@ -35,37 +35,55 @@ const initPending = [
 ];
 const initExcludeColumns = ["Post Date", "Category", "Type", "Memo"];
 
+export const file = signal(null);
+
 export default function FileProcessor() {
   const [ matchers, setMatchers ] = useState({ confirmed: [...initConfirmed], pending: [...initPending], exclude: [] });
   const [ config, setConfig ] = useState({ excludeColumns: [...initExcludeColumns] })
   const [ dates, setDates ] = useState({ start: '', end: '' });
-  const [ inputFile, setInputFile ] = useState('');
+  const [inputFiles, setInputFiles] = useState(null);
+  const fileInputRef = useRef(null)
 
 
   const handleDateChange = e => setDates(currentDates => { 
     return { ...currentDates, [e.target.name]: e.target.value }
   });
 
-  const handleFileChange = e => e.target.files && setInputFile(e.target.files[0]);
+  const handleUpload = () => {
+    if(inputFiles && inputFiles.length) {
+      file.value = inputFiles[0];
+      setInputFiles(null);
+      fileInputRef.current.value = "";
+    }
+  }
  
+  /* Temporary Logging */
+  useEffect(() => {inputFiles && console.log("File ready for upload:", inputFiles[0].name)}, [inputFiles])
+  file.value && console.log("File uploaded:", file.value.name);
+  useEffect(() => {dates.start ? console.log("Start date applied:", dates.start) : console.log("Start date removed.")}, [dates.start])
+  useEffect(() => {dates.end ? console.log("End date applied:", dates.end) : console.log("End date removed.")}, [dates.end])
+  /* Temporary Logging */
 
   return (
     <>
       <Stack direction="horizontal" gap={3}>
         <Form.Group controlId="statementCsv" >
           <Form.Label><b>Step 1.</b> Upload your credit card statement</Form.Label>
-          <Form.Control type="file" onChange={handleFileChange} />
+          <InputGroup>
+            <Form.Control ref={fileInputRef} type='file' onChange={(e) => setInputFiles(e.target.files)} />
+            {inputFiles && inputFiles.length ? <Button variant='primary' onClick={handleUpload}>Upload</Button> : <Button variant='outline-secondary' disabled>Upload</Button>}
+          </InputGroup>
           <Form.Text className='text-muted'>.csv format only</Form.Text>
         </Form.Group>
-        <Form.Group controlId="dateRange" >
+        <Form.Group id="date-range" >
           <Form.Label><b>Step 2.</b> Choose start and end date </Form.Label>
           <Stack direction='horizontal' gap={3}>
             <Stack>
-              <Form.Control type="date" value={dates.start} name="start" onChange={handleDateChange} />
+              <Form.Control id="start-date" type="date" value={dates.start} name="start" onChange={handleDateChange} />
               <Form.Text className="text-muted p2" >Start Date</Form.Text>
             </Stack>
             <Stack>
-              <Form.Control type="date" values={dates.end} name="end" onChange={handleDateChange} />
+              <Form.Control id="end-date" type="date" values={dates.end} name="end" onChange={handleDateChange} />
               <Form.Text className="text-muted p2">End Date</Form.Text>
             </Stack>
           </Stack>
