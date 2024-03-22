32922e5d7fc0a409795695a32993da064b976a57
leaky3 <crtotoro@gmail.com>
2024-03-22 14:09:03 -0500

    Handle file upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,56 @@
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
+import { InputGroup } from 'react-bootstrap';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  
+  const [inputFiles, setInputFiles] = useState(null);
+  const [file, setFile] = useState(null);
+  const fileInputRef = useRef(null)
+
+  const handleUpload = () => {
+    if(inputFiles && inputFiles.length) {
+      setFile(inputFiles[0]);
+      setInputFiles(null);
+      fileInputRef.current.value = "";
+    }
+    
+  }
+
+  useEffect(() => {inputFiles && console.log("File ready for upload:", inputFiles[0].name)}, [inputFiles])
+  useEffect(() => {file && console.log("File uploaded:", file.name)}, [file])
 
   return (
-    <>
+    <div className='m-3'>
       <h1>Settle Up</h1>
       <p>Split finances made easy</p>
-      <Stack direction="horizontal" gap={3}>
-        <Form.Group controlId="statementCsv" >
+      <Stack direction='horizontal' gap={3}>
+        <Form.Group >
           <Form.Label><b>Step 1.</b> Upload your credit card statement</Form.Label>
-          <Form.Control type="file" />
+          <InputGroup>
+            <Form.Control ref={fileInputRef} type='file' onChange={(e) => setInputFiles(e.target.files)} />
+            {inputFiles && inputFiles.length ? <Button variant='primary' onClick={handleUpload}>Upload</Button> : <Button variant='outline-secondary' disabled>Upload</Button>}
+          </InputGroup>
           <Form.Text className='text-muted'>.csv format only</Form.Text>
         </Form.Group>
-        <Form.Group controlId="dateRange" >
+        <Form.Group id='date-range'>
           <Form.Label><b>Step 2.</b> Choose start and end date </Form.Label>
           <Stack direction='horizontal' gap={3}>
             <Stack>
-              <Form.Control controlId="startDate" type="date" />
-              <Form.Text className="text-muted p2" >Start Date</Form.Text>
+              <Form.Control id='start-date' type='date'/>
+              <Form.Text className='text-muted p2' >Start Date</Form.Text>
             </Stack>
             <Stack>
-              <Form.Control controlId="endDate" type="date" />
-              <Form.Text className="text-muted p2">End Date</Form.Text>
+              <Form.Control id='end-date' type='date' />
+              <Form.Text className='text-muted p2'>End Date</Form.Text>
             </Stack>
           </Stack>
         </Form.Group>
 
       </Stack>
 
-    </>
+    </div>
   )
 }
 
