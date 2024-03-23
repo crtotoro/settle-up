9b046de24d2da433e0116a8e2b0837498bf6c528
leaky3 <crtotoro@gmail.com>
2024-03-23 13:26:47 -0500

    Update settings layout

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { Accordion, FloatingLabel, Form, InputGroup, Stack } from 'react-bootstrap'
+import { Accordion, AccordionBody, FloatingLabel, Form, InputGroup, Stack } from 'react-bootstrap'
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Settings() {
@@ -18,23 +18,55 @@ export default function Settings() {
   }, []);
   
   return (
-    <div className='settings p-2 d-flex flex-column align-items-center gap-1'>
-      <h5>Settings</h5>
-      <Form className='d-flex flex-column'>
-        <Form.Group id="date-range" className='d-flex gap-3' >
-            <Form.Label>Range</Form.Label>
-            <Form.Control id="start-date" type="date" value={dates.start} name="start" onChange={handleDateChange} data-bs-toggle="tooltip" data-bs-placement="bottom" title="Start Date"/>
-            <Form.Control id="end-date" type="date" value={dates.end} name="end" onChange={handleDateChange} data-bs-toggle="tooltip" data-bs-placement="bottom" title="End Date"/>
-        </Form.Group>
-      </Form>
-    </div>
+    <Accordion defaultActiveKey={0}>
+      <Accordion.Item eventKey={0}>
+        <Accordion.Header>Settings</Accordion.Header>
+        <Accordion.Body>
+          <Form className='settings p-2 d-flex flex-column align-items-start gap-2'>
+            <div className='settings-group p-3'>
+              <Form.Label>Date Range</Form.Label>
+              <Form.Control id="start-date" className='col' type="date" value={dates.start} name="start" onChange={handleDateChange} data-bs-toggle="tooltip" data-bs-placement="top" title="Start Date"/>
+              <Form.Control id="end-date" className='col' type="date" value={dates.end} name="end" onChange={handleDateChange} data-bs-toggle="tooltip" data-bs-placement="top" title="End Date"/>
+            </div>
+            <div className='settings-group p-3'>
+              <Form.Label>Transaction Matchers</Form.Label>
+              <Form.Group id="matchers" className='row' >
+                <Form.Group className='col'>
+                  <FloatingLabel controlId='include-matcher' label='Include if contains'>
+                    <Form.Control type="text" name="include" placeholder='Text...'/>
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className='col'>
+                  <FloatingLabel controlId='exclude-matcher' label='Exclude if contains'>
+                    <Form.Control type="text" name="exclude" placeholder='Text...'/>
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className='col'>
+                  <FloatingLabel controlId='verify-text-matcher' label='Verify if contains' >
+                    <Form.Control type="text" name="verify-text" placeholder='Text...'/>
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className='col'>
+                  <InputGroup>
+                    <InputGroup.Text>$</InputGroup.Text>
+                    <FloatingLabel controlId='verify-amount-matcher' label='Verify minimum'>
+                      <Form.Control type="number" name="verify-amount" min='0' step='5' placeholder='Amount...'/>
+                    </FloatingLabel>
+                  </InputGroup>
+                </Form.Group>
+              </Form.Group>
+            </div>
+          </Form>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   )
 }
 /* Todo
  * - Settings Layout
  *   - Default payor toggle -> primary or secondary
  *   - Default resolution -> split vs 100% owed
- *   - Add/View Matchers
+ *   - View Matchers
  *     - Include
  *     - Verify
  *     - Exclude
