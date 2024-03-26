258d3a0df0c1521e6331ce0c38b8adacfaec85f0
leaky3 <crtotoro@gmail.com>
2024-03-25 23:01:21 -0500

    Update date tooltips to use react bootstrap OverlayTrigger and Tooltip components instead of standard bootstrap tooltip

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useReducer } from 'react'
-import { Accordion, AccordionBody, FloatingLabel, Form, InputGroup, Offcanvas, Stack } from 'react-bootstrap'
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import React, { useState, useReducer } from 'react'
+import { FloatingLabel, Form, InputGroup, Offcanvas, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import matcherReducer from '../state/reducers/matcherReducer.js';
 import { initMatchers } from '../state/init/initMatchers.js';
 import TextMatcher from './TextMatcher.jsx';
@@ -15,13 +14,10 @@ export default function Settings({ showSettings, handleHideSettings }) {
     return { ...currentDates, [e.target.name]: e.target.value }
   });
 
-  /* Tooltips*/
-  useEffect(() => {
-    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new window.bootstrap.Tooltip(tooltipTriggerEl);
-    });
-  }, []);
+  /* Tooltips */
+  const renderTooltip = (props, id, text) => (
+    <Tooltip id={id} {...props}>{text}</Tooltip>
+  );
   
   return (
     <Offcanvas show={showSettings} onHide={handleHideSettings} placement='end'>
@@ -30,14 +26,22 @@ export default function Settings({ showSettings, handleHideSettings }) {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Form className='settings d-flex flex-column align-items-start gap-2'>
-          <div className='settings-group p-2 row'>
-            <Form.Label>Date Range</Form.Label>
-            <Form.Control id="start-date" className='col mx-2' type="date" value={dates.start} name="start" onChange={handleDateChange} data-bs-toggle="tooltip" data-bs-placement="bottom" title="Start Date"/>
-            <Form.Control id="end-date" className='col mx-2' type="date" value={dates.end} name="end" onChange={handleDateChange} data-bs-toggle="tooltip" data-bs-placement="bottom" title="End Date"/>
+          <div id='dates' className='settings-group p-2 row'>
+            <Form.Group className='col-12 mb-2 '>
+              <Form.Label>Date Range</Form.Label>
+              <div className='d-flex gap-2'>
+                <OverlayTrigger placement='left' overlay={(props) => renderTooltip(props, 'start-date-tip', 'Start Date')}>
+                  <Form.Control id="start-date" type="date" value={dates.start} name="start" onChange={handleDateChange}/>
+                </OverlayTrigger>
+                <OverlayTrigger placement='top' overlay={(props) => renderTooltip(props, 'end-date-tip', 'End Date')}>
+                  <Form.Control id="end-date" type="date" value={dates.end} name="end" onChange={handleDateChange}/>
+                </OverlayTrigger>
+              </div>
+              <Form.Text muted>Exclude transactions outside this range</Form.Text>
+            </Form.Group>
           </div>
-          <div className='settings-group p-2 row'>
+          <div id='matchers' className='settings-group p-2 row'>
             <Form.Label>Transaction Matchers</Form.Label>
-            {/* <Form.Group id="matchers" className='row' > */}
             {Object.keys(textMatchers).map(key => 
               <TextMatcher 
                 key={textMatchers[key].type} 
@@ -53,15 +57,28 @@ export default function Settings({ showSettings, handleHideSettings }) {
                 </FloatingLabel>
               </InputGroup>
             </Form.Group>
-            {/* </Form.Group> */}
+          </div>
+          <div id='settlement' className='settings-group p-2 row'>
+            <Form.Label>Settlement Assumptions</Form.Label>
+            <Form.Group className='col-12 mb-2'>
+              <FloatingLabel controlId='default-payor' label='Original payor'>
+                <Form.Select>
+                  <option value={1}>Participant1</option>
+                  <option value={2}>Participant2</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className='col-12 mb-2'>
+              <FloatingLabel controlId='default-settlement' label='How to split'>
+                <Form.Select>
+                  <option value={1}>50/50 Split</option>
+                  <option value={2}>100% Repaid</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Form.Group>
           </div>
         </Form>
       </Offcanvas.Body>
     </Offcanvas>
   )
 }
-/* Todo
- * - Settings Layout
- *   - Default payor toggle -> primary or secondary
- *   - Default resolution -> split vs 100% owed
- */
