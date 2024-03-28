72f682ad1341c98f3d1f3ec43c5787e245aa87ea
leaky3 <crtotoro@gmail.com>
2024-03-27 22:51:24 -0500

    Add Participants component missing from prior commit

---
+++ src/components/Participants.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Col, Form, InputGroup, OverlayTrigger } from 'react-bootstrap'
+import InputGroupText from 'react-bootstrap/esm/InputGroupText'
+import { useApp, handleFieldChange } from '../state/context/Context'
+
+export default function Participants() {
+  const { participants, setParticipants } = useApp();
+
+
+
+  return (
+    <Form.Group id='participants-form' className='py-3'>
+      <Form.Label><b>Step 2.</b> Who is splitting the expenses?</Form.Label>
+      <div className='participants d-flex gap-2'>
+        <InputGroup id='participant1' label='Primary'>
+          <InputGroup.Text>P1</InputGroup.Text>
+          <Form.Control type='text' name='p1' value={participants.p1} onChange={(e) => handleFieldChange(e, setParticipants)} placeholder='No name entered'/>
+        </InputGroup>
+        <InputGroup id='participant2' label='Secondary'>
+          <InputGroup.Text>P2</InputGroup.Text>
+          <Form.Control type='text' name='p2' value={participants.p2} onChange={(e) => handleFieldChange(e, setParticipants)} placeholder='No name entered' />
+        </InputGroup>
+      </div>
+    </Form.Group>
+  )
+}
