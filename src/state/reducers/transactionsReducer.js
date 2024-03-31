08cf3ffb124775f60de28bcb484bcd68b6583102
leaky3 <crtotoro@gmail.com>
2024-03-31 11:39:29 -0500

    Add revised transactionsReducer skeleton

--- src/state/reducers/transactionsReducer.js
+++ src/state/reducers/transactionsReducer.js
@@ -2,21 +2,25 @@ import { randomUUID } from "../../utils/helpers";
 
 export default function transactionsReducer(state, action) {
   switch(action.type) {
+    /* run on new file upload - need to split initial state into 
+      {
+        allTransactions: [],
+        toVerify: [],
+        toInclude: [],
+        toExclude: []
+      }
+    */
     case 'SET_TRANSACTIONS': {
       return action.payload
         .filter(transaction => transaction["Description"] && transaction["Amount"] < 0) // exclude empty rows and credit card payments
         .map(transaction => ({...transaction, "Amount": -1 * transaction["Amount"], id: randomUUID()})); // invert amounts to be positive;
     }
-    case 'GET_ALL_TRANSACTIONS': {
+    // run this when modifying a single transaction from one status to another
+    case 'UPDATE_TRANSACTION_STATUS': {
       return state;
     }
-    case 'GET_TRANSACTIONS_TO_INCLUDE': {
-      return state;
-    }
-    case 'GET_TRANSACTIONS_TO_EXCLUDE': {
-      return state;
-    }
-    case 'GET_TRANSACTIONS_TO_VERIFY': {
+    // run this after updating matcher filters
+    case 'APPLY_NEW_FILTERS': {
       return state;
     }
     default: {
